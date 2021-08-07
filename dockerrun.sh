a24c814f8b8cd156ea046162f22bba989a274667
MTsuyama-git <miyabi-yamabiko@hotmail.co.jp>
2021-08-07 12:39:51 +0900

    update for windows

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --user 1000 -e DOTNET_CLI_HOME=${HOME}/.dotnet -v ${HOME}:${HOME} -v ${PWD}:${PWD} -w ${PWD} dotnet:v1.0 $*
+docker run -it --rm --user 1000 -e DOTNET_CLI_HOME=/${HOME}/.dotnet -v /${HOME}:/${HOME} -v /${PWD}:/${PWD} -w /${PWD} dotnet:v1.0 $*
